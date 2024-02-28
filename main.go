bbd665033613118e1fd664d260682bb9b8430bca
bumkey <jsbebe02@acornsoft.io>
2024-02-28 15:06:54 +0900

    log-proxy - 2차개발 (리팩토링)

--- main.go
+++ main.go
@@ -1,143 +1,24 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"strconv"
-	"sync"
-	"time"
 
-	"Goproxy/util"
+	"Goproxy/proxy"
 )
 
-var customTransport = http.DefaultTransport
+func main() {
+	// configFile := "config.json"
+	configFile := "/var/conf/config.json"
 
-var configFileMutex sync.Mutex
-
-var configFile = "/var/conf/config.json"
-
-// var configFile = "config.json"
-
-var startTime = time.Now()
-
-func init() {
-	// Here, you can customize the transport, e.g., set timeouts or enable/disable keep-alive
-}
-
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	// 헤더에서 Account-Seq 추출
-	accountSeq := r.Header.Get("Account-Seq")
-	userID := r.Header.Get("user-id")
-	userRole := r.Header.Get("user-role")
-
-	fmt.Println("accountSeq: ", accountSeq, "userId: ", userID, "userRole: ", userRole)
-	fmt.Println("----------------------")
-
-	if accountSeq == "" {
-		http.Error(w, "unauthorized", http.StatusUnauthorized)
-	}
-
-	seq, err := strconv.Atoi(accountSeq)
-	if err != nil {
-		http.Error(w, "Invalid Account-Seq header", http.StatusBadRequest)
-		return
-	}
-
-	configFileMutex.Lock()
-	defer configFileMutex.Unlock()
-
-	// Get enabled URLs based on Account-Seq
-	enabledURLs, err := util.GetEnabledURLs(configFile, seq)
-	if err != nil {
-		http.Error(w, "Error getting enabled URLs", http.StatusInternalServerError)
-		return
-	}
-
-	var targetURL string
-	if len(enabledURLs) > 0 {
-		// 첫번째 객체만 반환 => 추후 바꿔야함
-		targetURL = enabledURLs[0]
-
-		// Add Path from the original request URL
-		// targetURL += r.URL.Path
-
-		if r.URL.RawQuery != "" {
-			targetURL += "?" + r.URL.RawQuery
-		}
-	} else {
-		http.Error(w, "No enabled URLs found for the given Account-Seq", http.StatusInternalServerError)
-		return
-	}
-
-	// Logging request message
-	// fmt.Println("Request URL:", r.URL)
-
-	proxyReq, err := http.NewRequest(r.Method, targetURL, r.Body)
-	if err != nil {
-		http.Error(w, "Error creating proxy request", http.StatusInternalServerError)
-		return
-	}
-
-	// Copy the headers from the original request to the proxy request
-	for name, values := range r.Header {
-		for _, value := range values {
-			proxyReq.Header.Add(name, value)
-		}
-	}
-
-	// Send the proxy request using the custom transport
-	resp, err := customTransport.RoundTrip(proxyReq)
-	if err != nil {
-		fmt.Println("Error sending proxy request:", err.Error())
-		http.Error(w, "Error sending proxy request", http.StatusInternalServerError)
-		return
-	}
-	defer resp.Body.Close()
-
-	// Logging response status code
-	// Calculate elapsed time
-	elapsed := time.Since(startTime)
-
-	// Log response details similar to nginx log_format
-	log.Printf(
-		"%s - - [%s] \"%s\" %d %d \"%s\" \"%s\" \"%s\" %.3fms\n",
-		r.RemoteAddr,
-		targetURL,
-		r.Method+" "+r.URL.Path+" "+r.Proto,
-		resp.StatusCode,
-		resp.ContentLength,
-		r.Referer(),
-		r.UserAgent(),
-		r.Header.Get("X-Forwarded-For"),
-		float64(elapsed.Microseconds())/1000000.0,
+	http.HandleFunc(
+		"/", func(w http.ResponseWriter, r *http.Request) {
+			proxy.HandleRequest(w, r, configFile)
+		},
 	)
 
-	// Copy the headers from the proxy response to the original response
-	for name, values := range resp.Header {
-		for _, value := range values {
-			w.Header().Add(name, value)
-		}
-	}
-
-	// Set the status code of the original response to the status code of the proxy response
-	w.WriteHeader(resp.StatusCode)
-
-	// Copy the body of the proxy response to the original response
-	io.Copy(w, resp.Body)
-}
-
-func main() {
-	// Create a new HTTP server with the handleRequest function as the handler
-	server := http.Server{
-		Addr:    ":9999",
-		Handler: http.HandlerFunc(handleRequest),
-	}
-
-	// Start the server and log any errors
 	log.Println("Starting proxy server on :9999")
-	err := server.ListenAndServe()
+	err := http.ListenAndServe(":9999", nil)
 	if err != nil {
 		log.Fatal("Error starting proxy server: ", err)
 	}
---
+++ proxy/proxy.go
@@ -0,0 +1,135 @@
+package proxy
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+var customTransport = http.DefaultTransport
+
+var configFileMutex sync.Mutex
+
+var startTime = time.Now()
+
+type AccountInfo struct {
+	Name    string `json:"name"`
+	URL     string `json:"url"`
+	Enabled bool   `json:"enabled"`
+}
+
+func GetEnabledURLs(filePath string, seq int) ([]string, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var accounts map[string][]AccountInfo
+	if err := json.Unmarshal(data, &accounts); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal JSON: %v", err)
+	}
+
+	var enabledURLs []string
+	seqStr := strconv.Itoa(seq)
+	for accountId, accountInfo := range accounts {
+		if strings.HasSuffix(accountId, seqStr) {
+			for _, info := range accountInfo {
+				if info.Enabled {
+					enabledURLs = append(enabledURLs, info.URL)
+				}
+			}
+		}
+	}
+	return enabledURLs, nil
+}
+
+func HandleRequest(w http.ResponseWriter, r *http.Request, configFile string) {
+	accountSeq := r.Header.Get("Account-Seq")
+	userID := r.Header.Get("user-id")
+	userRole := r.Header.Get("user-role")
+
+	fmt.Println("accountSeq: ", accountSeq, "userId: ", userID, "userRole: ", userRole)
+	fmt.Println("----------------------")
+
+	if accountSeq == "" {
+		http.Error(w, "unauthorized", http.StatusUnauthorized)
+	}
+
+	seq, err := strconv.Atoi(accountSeq)
+	if err != nil {
+		http.Error(w, "Invalid Account-Seq header", http.StatusBadRequest)
+		return
+	}
+
+	configFileMutex.Lock()
+	defer configFileMutex.Unlock()
+
+	enabledURLs, err := GetEnabledURLs(configFile, seq)
+	if err != nil {
+		http.Error(w, "Error getting enabled URLs", http.StatusInternalServerError)
+		return
+	}
+
+	var targetURL string
+	if len(enabledURLs) > 0 {
+		targetURL = enabledURLs[0]
+		if r.URL.RawQuery != "" {
+			targetURL += "?" + r.URL.RawQuery
+		}
+	} else {
+		http.Error(w, "No enabled URLs found for the given Account-Seq", http.StatusInternalServerError)
+		return
+	}
+
+	proxyReq, err := http.NewRequest(r.Method, targetURL, r.Body)
+	if err != nil {
+		http.Error(w, "Error creating proxy request", http.StatusInternalServerError)
+		return
+	}
+
+	for name, values := range r.Header {
+		for _, value := range values {
+			proxyReq.Header.Add(name, value)
+		}
+	}
+
+	resp, err := customTransport.RoundTrip(proxyReq)
+	if err != nil {
+		fmt.Println("Error sending proxy request:", err.Error())
+		http.Error(w, "Error sending proxy request", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	elapsed := time.Since(startTime)
+
+	log.Printf(
+		"%s - - [%s] \"%s\" %d %d \"%s\" \"%s\" \"%s\" %.3fms\n",
+		r.RemoteAddr,
+		targetURL,
+		r.Method+" "+r.URL.Path+" "+r.Proto,
+		resp.StatusCode,
+		resp.ContentLength,
+		r.Referer(),
+		r.UserAgent(),
+		r.Header.Get("X-Forwarded-For"),
+		float64(elapsed.Microseconds())/1000000.0,
+	)
+
+	for name, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(name, value)
+		}
+	}
+
+	w.WriteHeader(resp.StatusCode)
+
+	io.Copy(w, resp.Body)
+}
